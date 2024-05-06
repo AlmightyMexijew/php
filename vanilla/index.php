70c3c5550eac30305926449c0327b0b92546bcf9
almightymexijew <ctepper625@gmail.com>
2024-05-06 12:02:44 +0300

    dir for vanilla php

---
+++ vanilla/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+    </head>
+    <body>
+        <h1>My first PHP page</h1>
+        <?php
+        echo "Hello World!";
+        #this is our first echo/write to page :)
+        //And this is our second comment
+        /*Because we can comment in all 3 of these methods but only this one is multi-line*/
+        $varone = "Hello";
+        $vartwo = "World!";
+        echo $varone;
+        //This seems to achieve little.
+        ?>
+    </body>
+</html>
